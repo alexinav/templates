0eb6777e1a33edb7d1d8ec27703ac29fc27f1b0b
Alex Nazarenko <ais.jaz@hotmail.com>
2020-06-11 01:14:54 +0200

    exec

--- to call.ps1
+++ to call.ps1
@@ -1,6 +1,4 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-
-
 ./deploy.ps1 -subscriptionId "<subscriptionId>" -resourceGroupName "<ResouceGroup>" -deploymentName "azuredeployname01"
 
